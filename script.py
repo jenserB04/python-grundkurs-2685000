9f89de6e65f24a1d11a1abb61237cda797f0501a
jenserB04 <jbobbert@uni-muenster.de>
2025-10-22 09:24:55 +0000

    script.py

--- script.py
+++ script.py
@@ -4,3 +4,21 @@ print("Hello, World!")
 
 zahl = 5
 print(zahl)
+
+list =[1,2,3,4]
+list.append("a")
+print("Meine Liste: ", list)
+
+list[0]=100
+print("Meine Liste: ", list)
+print("Länge", len(list))
+
+cast_to_str = str(list[0])
+print(cast_to_str)
+
+dict ={"hallo": 1, "zwei": "wei"}
+print(dict)
+
+wert = 5
+wandel = float(wert)
+print(wandel)
