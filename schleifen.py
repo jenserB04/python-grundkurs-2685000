d9235480303a806f32cbd5bfc036fa3841a2f427
jenserB04 <jbobbert@uni-muenster.de>
2025-10-23 09:37:03 +0000

    schleifen hinzugefügt

---
+++ schleifen.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Schleifen in Python
+
+# for-Schleife: Iteration über eine Liste von Zahlen
+zahlen = [1, 2, 3, 4, 5]
+for zahl in zahlen:
+    print("Zahl aus der Liste:", zahl)
+
+for i in range(1,6):
+    print(i)
+
+x=13
+while x>5:
+  print(x)
+  x-=1
+  if x<=5:
+        print("x ist kleiner gleich 5")
+
+for i in range(1,11):
+    print(i)
+z=10
+while z>0:
+    print(z)
+    z-=1
